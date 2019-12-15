ba1ccea0d3af20895874014ec458eeec242c531f
Vic Wu <madcocomo@gmail.com>
2019-12-15 09:11:12 +0800

    empty message

--- src/main/java/com/sap/ase/IsomorphicChecker.java
+++ src/main/java/com/sap/ase/IsomorphicChecker.java
@@ -10,21 +10,22 @@ public class IsomorphicChecker {
             return true;
 
         for(int i=0; i< s.length()-1; i++){
-            for (int j = 1; j < s.length(); j++) {
-                if (isSameToNext(s, t, i, j)) return false;
+            if (s.charAt(i) == s.charAt(i+1) && t.charAt(i) != t.charAt(i+1))
+                return false;
+
+            if (s.charAt(i) != s.charAt(i+1) && t.charAt(i) == t.charAt(i+1))
+                return false;
+            if (i < s.length() - 2) {
+                if (s.charAt(i) == s.charAt(i + 2) && t.charAt(i) != t.charAt(i + 2))
+                    return false;
+
+                if (s.charAt(i) != s.charAt(i + 2) && t.charAt(i) == t.charAt(i + 2))
+                    return false;
             }
             return true;
         }
         return false;
 
 
-    }
-
-    private static boolean isSameToNext(String s, String t, int i, int i2) {
-        if (s.charAt(i) == s.charAt(i + i2) && t.charAt(i) != t.charAt(i + i2))
-            return true;
-        if (s.charAt(i) != s.charAt(i + i2) && t.charAt(i) == t.charAt(i + i2))
-            return true;
-        return false;
     }
 }
