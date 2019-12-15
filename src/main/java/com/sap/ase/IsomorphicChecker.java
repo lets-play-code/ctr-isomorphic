412969e1c2c24d6ce69433b5a4d0fe4c7b41ccc0
Vic Wu <madcocomo@gmail.com>
2019-12-15 09:18:07 +0800

    empty message

--- src/main/java/com/sap/ase/IsomorphicChecker.java
+++ src/main/java/com/sap/ase/IsomorphicChecker.java
@@ -16,7 +16,7 @@ public class IsomorphicChecker {
     }
 
     private static boolean isNotSameAt(String s, String t, int i, int i2, int i1) {
-        if (s.charAt(i) == s.charAt(i1) && t.charAt(i) != t.charAt(i + i2))
+        if (s.charAt(i) == s.charAt(i1) && t.charAt(i) != t.charAt(i1))
             return true;
         if (s.charAt(i) != s.charAt(i1) && t.charAt(i) == t.charAt(i1))
             return true;
