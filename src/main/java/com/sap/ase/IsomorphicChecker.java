d46933011161e9a6aea15dc3cde18fd8c3847d7d
Vic Wu <madcocomo@gmail.com>
2019-12-15 09:36:25 +0800

    empty message

--- src/main/java/com/sap/ase/IsomorphicChecker.java
+++ src/main/java/com/sap/ase/IsomorphicChecker.java
@@ -2,10 +2,8 @@ package com.sap.ase;
 
 public class IsomorphicChecker {
     public static boolean comparePattern(String s, String t) {
-        if (s==null||t==null)
-            return false;
-        if (s.length() != t.length())
-            return false;
+        if (s==null||t==null) return false;
+        if (s.length() != t.length()) return false;
         for(int i=0; i< s.length()-1; i++){
             for (int j = i+1; j < s.length(); j++) {
                 if (isSameChar(s, i, j) != isSameChar(t, i, j)) return false;
