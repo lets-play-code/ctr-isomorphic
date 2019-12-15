62703c263db3e8c33029304237a4a7e78bf4214c
Vic Wu <madcocomo@gmail.com>
2019-12-15 09:33:43 +0800

    empty message

--- src/main/java/com/sap/ase/IsomorphicChecker.java
+++ src/main/java/com/sap/ase/IsomorphicChecker.java
@@ -8,7 +8,7 @@ public class IsomorphicChecker {
             return false;
         for(int i=0; i< s.length()-1; i++){
             for (int j = i+1; j < s.length(); j++) {
-                if (isSameChar(s, i, j) == !isSameChar(t, i, j)) return false;
+                if (isSameChar(s, i, j) != isSameChar(t, i, j)) return false;
             }
         }
         return true;
