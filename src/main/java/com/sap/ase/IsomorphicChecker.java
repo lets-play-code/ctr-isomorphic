bcab15f08ffc111f1b5e8ba01f2fe0e2b2d65221
Vic Wu <madcocomo@gmail.com>
2019-12-17 08:34:35 +0800

    init version before refactor

--- src/main/java/com/sap/ase/IsomorphicChecker.java
+++ src/main/java/com/sap/ase/IsomorphicChecker.java
@@ -1,4 +1,39 @@
 package com.sap.ase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsomorphicChecker {
+    public boolean isIsomorphic(String s, String t) {
+        if (s == null || s == null)
+            return false;
+        if (s == t || s.equals(t))
+            return true;
+
+        if (s.length() != t.length())
+            return false;
+
+        Map<String, String> charMapS2T = new HashMap<>();
+        Map<String, String> charMapT2S = new HashMap<>();
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        for (int i = 0; i < sArray.length; i++) {
+            char sChar = sArray[i];
+            char tChar = tArray[i];
+
+            if (charMapS2T.get(String.valueOf(sChar)) != null && !charMapS2T.get(String.valueOf(sChar)).equals(String.valueOf(tChar))) {
+                return false;
+            }
+            if (charMapT2S.get(String.valueOf(tChar)) != null && !charMapT2S.get(String.valueOf(tChar)).equals(String.valueOf(sChar))) {
+                return false;
+            }
+
+            if (charMapS2T.get(String.valueOf(sChar)) == null) {
+                charMapS2T.put(String.valueOf(sChar), String.valueOf(tChar));
+                charMapT2S.put(String.valueOf(tChar), String.valueOf(sChar));
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/com/sap/ase/IsomorphicStringTest.java
+++ src/test/java/com/sap/ase/IsomorphicStringTest.java
@@ -2,14 +2,34 @@ package com.sap.ase;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.stream.Stream;
+
+import static com.sap.ase.IsomorphicChecker.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class IsomorphicStringTest {
 
-	@Test
-	public void testName() throws Exception {
-	    assertTrue(true, "start point");
+	@ParameterizedTest
+	@MethodSource("args")
+	public void testSame(String s1, String s2, boolean expected) throws Exception {
+		assertEquals(expected, new IsomorphicChecker().isIsomorphic(s1, s2));
+	}
 
+	static Stream<Arguments> args () {
+		return Stream.of(
+				Arguments.of(null, "abcd", false),
+				Arguments.of("egg", "abcd", false),
+				Arguments.of("aa", "bb", true),
+				Arguments.of("aa", "bc", false),
+				Arguments.of("ab", "cd", true),
+				Arguments.of("ada", "bob", true),
+				Arguments.of("ada", "tom", false),
+				Arguments.of("egg", "abc", false),
+				Arguments.of("egg", "add", true)
+		);
 	}
 }
