3cef8e32e5beb0b3597ae7e8aec71416c9bf86ba
Vic Wu <madcocomo@gmail.com>
2021-06-30 21:53:05 +0800

    empty message

--- src/test/java/com/sap/ase/DatePrefixTest.java
+++ src/test/java/com/sap/ase/DatePrefixTest.java
@@ -1,14 +1,31 @@
 package com.sap.ase;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class DatePrefixTest {
+    private static Stream<Arguments> testDatas() {
+        return Stream.of(
+                Arguments.of(LocalDate.of(2021, 6, 30), LocalDate.of(2021, 6, 30))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testDatas")
+    void testDatePrefix(LocalDate from, LocalDate to) {
+        LocalDate date = LocalDate.of(2021, 6, 30);
+        assertEquals(Arrays.asList("2021-06-30"), DatePrefix.of(date, date));
+    }
+
     @Test
     void should_be_date_string_for_single_date() {
         LocalDate date = LocalDate.of(2021, 6, 30);
