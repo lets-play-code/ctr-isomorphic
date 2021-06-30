aba9d5fa986380892faaf8f2a176c1f3cfd75562
Vic Wu <madcocomo@gmail.com>
2021-06-30 17:53:32 +0800

    empty message

--- src/test/java/com/sap/ase/DatePrefix.java
+++ src/test/java/com/sap/ase/DatePrefix.java
@@ -1,9 +1,11 @@
 package com.sap.ase;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 public class DatePrefix {
-    public static String of(LocalDate of) {
-        return "2021-06-30";
+    public static List<String> of(LocalDate of) {
+        return Arrays.asList("2021-06-30");
     }
 }
--- src/test/java/com/sap/ase/DatePrefixTest.java
+++ src/test/java/com/sap/ase/DatePrefixTest.java
@@ -3,6 +3,7 @@ package com.sap.ase;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DatePrefixTest {
     @Test
     void should_be_date_string_for_single_date() {
-        assertEquals("2021-06-30", DatePrefix.of(LocalDate.of(2021,6,30)));
+        assertEquals(Arrays.asList("2021-06-30"), DatePrefix.of(LocalDate.of(2021,6,30)));
     }
 
 }
