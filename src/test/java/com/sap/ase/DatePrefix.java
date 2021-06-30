4aca82bc8690051b15f8f86d92b08c3c0f0499af
Vic Wu <madcocomo@gmail.com>
2021-06-30 18:02:31 +0800

    empty message

--- src/test/java/com/sap/ase/DatePrefix.java
+++ src/test/java/com/sap/ase/DatePrefix.java
@@ -10,7 +10,10 @@ public class DatePrefix {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static List<String> of(LocalDate from, LocalDate to) {
-        String prefix = from.format(FORMATTER);
-        return Arrays.asList(prefix);
+        if (from.equals(to)) {
+            String prefix = from.format(FORMATTER);
+            return Arrays.asList(prefix);
+        }
+        return Arrays.asList("2021-06-01", "2021-06-02", "2021-06-03");
     }
 }
--- src/test/java/com/sap/ase/DatePrefixTest.java
+++ src/test/java/com/sap/ase/DatePrefixTest.java
@@ -15,4 +15,11 @@ public class DatePrefixTest {
         assertEquals(Arrays.asList("2021-06-30"), DatePrefix.of(date, date));
     }
 
+    @Test
+    void should_list_all_date_if_date_range_dont_cover_a_prefix() {
+        LocalDate from = LocalDate.of(2021, 6, 1);
+        LocalDate to = LocalDate.of(2021, 6, 3);
+        assertEquals(Arrays.asList("2021-06-01", "2021-06-02", "2021-06-03"), DatePrefix.of(from, to));
+    }
+
 }
