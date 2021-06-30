55e0f92dff367d4f31d613c9618877be17e17bfd
Vic Wu <madcocomo@gmail.com>
2021-06-30 21:39:21 +0800

    empty message

--- src/test/java/com/sap/ase/DatePrefix.java
+++ src/test/java/com/sap/ase/DatePrefix.java
@@ -3,7 +3,6 @@ package com.sap.ase;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,13 +13,17 @@ public class DatePrefix {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static List<String> of(LocalDate from, LocalDate to) {
-        if (daysBetween(from, to) == 10) {
+        if (fulfilledPrefix(from, to)) {
             return Arrays.asList(toDateString(from).substring(0, 9));
         }
         return Stream.iterate(from, date -> date.plusDays(1))
                 .limit(daysBetween(from, to))
                 .map(DatePrefix::toDateString)
                 .collect(Collectors.toList());
+    }
+
+    private static boolean fulfilledPrefix(LocalDate from, LocalDate to) {
+        return daysBetween(from, to) == 10;
     }
 
     private static long daysBetween(LocalDate from, LocalDate to) {
