3fbce1ad3c4fd127b512ac47b638b72f90c2871e
Vic Wu <madcocomo@gmail.com>
2021-06-30 18:06:58 +0800

    empty message

--- src/test/java/com/sap/ase/DatePrefix.java
+++ src/test/java/com/sap/ase/DatePrefix.java
@@ -11,9 +11,6 @@ public class DatePrefix {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static List<String> of(LocalDate from, LocalDate to) {
-        if (from.equals(to)) {
-            return Arrays.asList(toDateString(from));
-        }
         List<String> prefixes = new ArrayList<>();
         for (LocalDate date = from; !date.isAfter(to); date = date.plusDays(1)) {
             prefixes.add(toDateString(date));
