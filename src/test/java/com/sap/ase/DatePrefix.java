24f4d22186c685ce1c8d4ec90646f9fa5db8f40c
Vic Wu <madcocomo@gmail.com>
2021-06-30 17:57:13 +0800

    empty message

--- src/test/java/com/sap/ase/DatePrefix.java
+++ src/test/java/com/sap/ase/DatePrefix.java
@@ -1,12 +1,14 @@
 package com.sap.ase;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
 public class DatePrefix {
     public static List<String> of(LocalDate from, LocalDate to) {
-        String prefix = "2021-06-30";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String prefix = from.format(formatter);
         return Arrays.asList(prefix);
     }
 }
