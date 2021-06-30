37e47f6bb99d94d4a36a82cd885a101f71db8da7
Vic Wu <madcocomo@gmail.com>
2021-06-30 17:55:05 +0800

    empty message

--- src/test/java/com/sap/ase/DatePrefix.java
+++ src/test/java/com/sap/ase/DatePrefix.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DatePrefix {
-    public static List<String> of(LocalDate of) {
+    public static List<String> of(LocalDate from, LocalDate to) {
         return Arrays.asList("2021-06-30");
     }
 }
