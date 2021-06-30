7d91de78e9c066496d21d7fd580cb73ad9c41fb0
Vic Wu <madcocomo@gmail.com>
2021-06-30 22:07:03 +0800

    empty message

--- src/test/java/com/sap/ase/DatePrefixTest.java
+++ src/test/java/com/sap/ase/DatePrefixTest.java
@@ -29,7 +29,9 @@ public class DatePrefixTest {
             Arguments.of(LocalDate.of(2021, 7, 30), LocalDate.of(2021, 7, 31),
                     Arrays.asList("2021-07-3"), "day 30 & 31"),
             Arguments.of(LocalDate.of(2021, 2, 20), LocalDate.of(2021, 2, 28),
-                    Arrays.asList("2021-02-2"), "normal year Feb 2x")
+                    Arrays.asList("2021-02-2"), "normal year Feb 2x"),
+            Arguments.of(LocalDate.of(2020, 2, 20), LocalDate.of(2020, 2, 29),
+                    Arrays.asList("2020-02-2"), "leap year Feb 2x")
         );
     }
 
