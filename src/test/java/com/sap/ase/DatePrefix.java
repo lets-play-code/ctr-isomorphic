b1f9389f736d5090b1b87d0ddcde0d1865311063
Vic Wu <madcocomo@gmail.com>
2021-06-30 18:18:30 +0800

    empty message

--- src/test/java/com/sap/ase/DatePrefix.java
+++ src/test/java/com/sap/ase/DatePrefix.java
@@ -2,20 +2,22 @@ package com.sap.ase;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DatePrefix {
 
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static List<String> of(LocalDate from, LocalDate to) {
-        List<String> prefixes = new ArrayList<>();
-        for (LocalDate date = from; !date.isAfter(to); date = date.plusDays(1)) {
-            prefixes.add(toDateString(date));
-        }
-        return prefixes;
+        return Stream.iterate(from, date -> date.plusDays(1))
+                .limit(ChronoUnit.DAYS.between(from, to) + 1)
+                .map(DatePrefix::toDateString)
+                .collect(Collectors.toList());
     }
 
     private static String toDateString(LocalDate from) {
